f25f6decf360c99c72535b20264f1e2cf541b8ca
Yutae Lee <dobidugi@gmail.com>
2025-02-25 20:52:05 +0900

    actuactor WriteOperation custom endpoint 실습

--- src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
+++ src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
@@ -1,15 +1,23 @@
 package com.dobi.springactuator.actuactor.endpotins;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @Endpoint(id = "myLibrary")
 public class MyLibraryEndPoint {
+
+    @WriteOperation
+    public void writeOperatorTest(String value) {
+        log.info("Write Operation: {}", value);
+    }
 
     @ReadOperation
     public List<MyLib> getMyLibraryInfos(@Nullable String name) {
