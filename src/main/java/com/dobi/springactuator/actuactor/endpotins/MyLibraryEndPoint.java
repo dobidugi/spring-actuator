cfcc602045f9dca0d536bd16f531a2949d7e7379
Yutae Lee <dobidugi@gmail.com>
2025-02-25 20:18:11 +0900

    actuactor ReadOperation custom endpoint의 param 추가

--- src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
+++ src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
@@ -2,6 +2,7 @@ package com.dobi.springactuator.actuactor.endpotins;
 
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,8 +12,14 @@ import java.util.List;
 public class MyLibraryEndPoint {
 
     @ReadOperation
-    public List<MyLib> getMyLibraryInfos() {
+    public List<MyLib> getMyLibraryInfos(@Nullable String name) {
+        List<MyLib> libs = List.of(new MyLib("Java", "8"), new MyLib("Spring", "5"));;
 
-        return List.of(new MyLib("Java", "8"), new MyLib("Spring", "5"));
+        if(name != null) {
+            libs = libs.stream()
+                    .filter(lib -> lib.getName().equals(name))
+                    .toList();
+        }
+        return libs;
     }
 }
