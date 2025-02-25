d6e968c113879a305725aa0a99834ec3f05fa0d9
Yutae Lee <dobidugi@gmail.com>
2025-02-25 21:06:03 +0900

    actuator custom endpoint 실습 끝

--- src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
+++ src/main/java/com/dobi/springactuator/actuactor/endpotins/MyLibraryEndPoint.java
@@ -3,10 +3,12 @@ package com.dobi.springactuator.actuactor.endpotins;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -17,6 +19,18 @@ public class MyLibraryEndPoint {
     @WriteOperation
     public void writeOperatorTest(String value) {
         log.info("Write Operation: {}", value);
+    }
+
+//    @ReadOperation
+//    public String getPathTest(@Selector String path1) {
+//        log.info("Read Operation: {}", path1);
+//        return "Path1: " + path1;
+//    }
+
+    @ReadOperation
+    public String getPathTest(@Selector(match = Selector.Match.ALL_REMAINING) String path1[]) {
+        log.info("path: {}", Arrays.asList(path1));
+        return "Path1: " + path1;
     }
 
     @ReadOperation
